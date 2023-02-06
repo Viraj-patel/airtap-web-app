9297601c78ffc867a63da77acb6aabd8aedb6ba5
Viraj Patel <62351373+Viraj-patel@users.noreply.github.com>
2023-02-06 21:47:43 +0530

    Update constants.js
    updated BE url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://34.203.38.7/api/";
+export const API_URL = "http://52.3.220.35/api/";
 
 export const CHECK_LOGIN = "checkLogin";
 export const USER_DETAILS = "getUserDetails";
