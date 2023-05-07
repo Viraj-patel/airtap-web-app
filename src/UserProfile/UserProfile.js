fb415eaa26529a674087f517ef6e66a60f1d94ae
Viraj Patel <patelviraj399@gmail.com>
2023-05-07 17:31:19 +0530

    updated user profile

--- src/UserProfile/UserProfile.js
+++ src/UserProfile/UserProfile.js
@@ -3,8 +3,8 @@ import { profileDetails } from "../Profile/profileUtils";
 import { useLocation } from "react-router-dom";
 import { API_URL, NEW_USER_DETAILS } from "../constants/constants";
 import axios from "axios";
-import { withRouter } from "react-router-dom";
 import _ from "lodash";
+import classes from "./UserProfile.module.css";
 
 function UserProfile() {
   const [customTemplate, setTemplate] = useState([]);
@@ -14,6 +14,7 @@ function UserProfile() {
 
   useEffect(() => {
     const id = _.last(location.pathname.split("/"));
+    console.log(API_URL + NEW_USER_DETAILS + id);
     axios
       .get(API_URL + NEW_USER_DETAILS, { params: { unique_id: id } })
       .then((res) => {
@@ -38,7 +39,17 @@ function UserProfile() {
       });
   }, []);
   console.log(customTemplate, profileDetails);
-  return <div>UserProfile</div>;
+  return (
+    <div className={classes.container}>
+      {_.map(customTemplate, (tmp) => (
+        <div className={classes.rowContainer}>
+          {_.map(tmp, (obj) => (
+            <div>col</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default UserProfile;
